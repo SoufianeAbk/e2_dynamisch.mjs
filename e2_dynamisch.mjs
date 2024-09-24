aa50c4ad5a57accced891faf603a4bb763e3098e
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2024-09-24 18:04:12 +0200

    e2_dynamisch.mjs

---
+++ e2_dynamisch.mjs
@@ -0,0 +1,20 @@
+// Begin van userInput voorbereiden
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+// Einde van userInput voorbereiden
+
+
+
+// Voeg hier je eigen code in
+let name = await userInput.question('Wat is je naam? ');
+console.log(name);
+let age = await userInput.question('Hoe oud ben je? ');
+console.log(age);
+let favoriteGame = await userInput.question('Wat is jou favoriete game? ');
+console.log(favoriteGame);
+
+
+console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
+
+process.exit();
